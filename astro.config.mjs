eed7914322558566a207eef5f9328c762863ae44
Thraka <thraka.junk@outlook.com>
2026-02-22 00:33:54 -0800

    Try to fix ignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -86,7 +86,7 @@ export default defineConfig({
                           },
                       ],
                   },
-              ], { exclude: ['guides/terminal-demo'] }),
+              ], { exclude: ['guides/terminal-demo/**'] }),
           ],
       }),
 	],
